96760f73b0a7f7976824376ea077d4009e749f20
yashms25 <yashmistry614@gmail.com>
2021-11-24 11:33:43 +0530

    router and models added

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,18 @@
 const connectToMongo = require("./db");
-const express = require('express')
+const express = require('express');
 connectToMongo();
 
 const app = express()
 const port = 3000
 
+app.use(express.json())
+// Available routes
 app.get('/', (req, res) => {
   res.send('Hello yash')
 })
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
