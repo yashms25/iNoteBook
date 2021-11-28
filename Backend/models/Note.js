59a11071dd2e57c13922745904611668ddee27fd
yashms25 <yashmistry614@gmail.com>
2021-11-28 19:39:19 +0530

    notes added and updated

--- Backend/models/Note.js
+++ Backend/models/Note.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
+  user:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user'
+  },
   title: {
     type: String,
     required: true,
@@ -20,4 +24,5 @@ const NotesSchema = new Schema({
   },
 });
 
-module.exports = mongoose.mmodel("notes", NotesSchema);
+const Notes = mongoose.model("notes", NotesSchema);
+module.exports = Notes
--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,9 +1,83 @@
 const express = require("express");
+const fetchuser = require("../middleware/fetchuser");
 const router = express.Router();
+const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
 
-router.get("/", (req, res) => {
-  
-  res.json({});
+//route: 1 get all the notes get 'api/notes/fetchallnotes. login required
+
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal server error occured");
+  }
+});
+
+//route: 2 Add a new note post 'api/notes/addnote. login required
+
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must be atleast 5 characters.").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const saveNote = await note.save();
+      res.json(saveNote);
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).send("Internal server error occured");
+    }
+  }
+);
+
+//route: 3 Update an existing note 'api/notes/updatenote. login required
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  // create new note object
+  const newNote = {};
+  if (title) {
+    newNote.title = title;
+  }
+  if (description) {
+    newNote.description = description;
+  }
+  if (tag) {
+    newNote.tag = tag;
+  }
+
+  //find note to be updated
+  let note = await Note.findById(req.params.id);
+  if (!note) {
+    return res.status(404).send("Not found");
+  }
+  if (note.user.toString() !== req.user.id) {
+    return res.status(401).send("Not allowed");
+  }
+  note = await Note.findByIdAndUpdate(
+    req.params.id,
+    { $set: newNote },
+    { new: true }
+  );
+  res.json({note})
 });
 
 module.exports = router;
