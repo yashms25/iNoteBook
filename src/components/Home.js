df24e500639e9df518d78d4b7325cdb68a744852
yashms25 <yashmistry614@gmail.com>
2021-11-30 19:22:46 +0530

    icon added

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
         <h2>Add a Note</h2>
         <form className="my-3">
           <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">
+            <label htmlFor="exampleInputEmail1" class="form-label">
               Email address
             </label>
             <input
@@ -22,7 +22,7 @@ export default function Home() {
             </div>
           </div>
           <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">
+            <label htmlFor="exampleInputPassword1" class="form-label">
               Password
             </label>
             <input
@@ -37,7 +37,7 @@ export default function Home() {
               class="form-check-input"
               id="exampleCheck1"
             />
-            <label class="form-check-label" for="exampleCheck1">
+            <label class="form-check-label" htmlFor="exampleCheck1">
               Check me out
             </label>
           </div>
--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -4,10 +4,14 @@ export default function Noteitem(props) {
   const { note } = props;
   return (
     <div className="col-md-4">
-      <div class="card my-3">
-        <div class="card-body">
-          <h5 class="card-title">{note.title}</h5>
-          <p class="card-text">{note.description}</p>
+      <div className="card my-3">
+        <div className="card-body">
+          <div className="d-flex align-items-center">
+            <h5 className="card-title">{note.title}</h5>
+            <i class="fa fa-trash-o mx-2" aria-hidden="true"></i>
+            <i class="fa fa-pencil-square-o mx-2" aria-hidden="true"></i>
+          </div>
+          <p className="card-text">{note.description}</p>
         </div>
       </div>
     </div>
